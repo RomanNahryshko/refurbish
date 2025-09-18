95c18c33bdf21dcb863b751f03f457cb26a5f2f1
DenisVasiliev-ag <108676297+DenisVasiliev-ag@users.noreply.github.com>
2025-09-18 11:02:01 +0300

    Update build.sh

--- docker/prod/build.sh
+++ docker/prod/build.sh
@@ -2,6 +2,8 @@
 
 export HUB_PWD=$1
 export COMPOSE_FILE=$2
+echo $COMPOSE_FILE
+pwd
 export BUILD_FLAGS=$3||''
 export DOCKER_HOST=tcp://95.217.158.135:2376
 export DOCKER_CERT_PATH='/certs/builder'
